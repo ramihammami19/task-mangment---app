0f4f5cfbb4288a87563f26fdd3f433ddc95a3a7e
ramihammami19 <ramihammami354@gmail.com>
2024-06-11 06:35:23 -0600

    first commit

---
+++ database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb://127.0.0.1:27017/task-manager';
+
+
+mongoose
+	.connect(mongoURI)
+	.then(() => {
+		console.log('Connected to MongoDB');
+		// Start your application or perform additional operations
+	})
+	.catch((error) => {
+		console.error('Error connecting to MongoDB:', error);
+	});
